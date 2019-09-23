a41eae4b4209f1d63160fba385c1b060904d5577
blackjack201312 <blackjack201303@gmail.com>
2019-09-23 08:25:43 +0800

    update B.

--- B/benchmark.sh
+++ B/benchmark.sh
@@ -1,20 +1,4 @@
-#dnn=vgg16 bs=64  epoch=1 ./single.sh
-#dnn=vgg16 bs=128 epoch=1 ./single.sh
-#dnn=vgg16 bs=256 epoch=1 ./single.sh
-
-#dnn=resnet50 bs=64  epoch=1 ./single.sh
-#dnn=resnet50 bs=128 epoch=1 ./single.sh
-#dnn=resnet50 bs=256 epoch=1 ./single.sh
-
-#dnn=inception_v3 bs=64  epoch=1 ./single.sh
-#dnn=inception_v3 bs=128 epoch=1 ./single.sh
-#dnn=inception_v3 bs=256 epoch=1 ./single.sh
-
-#dnn=lstm bs=64   epoch=1 ./single.sh
-#dnn=lstm bs=128  epoch=1 ./single.sh
-#dnn=lstm bs=256  epoch=1 ./single.sh
-
-dnn=lstman4 bs=8   epoch=1 ./single.sh
-dnn=lstman4 bs=16  epoch=1 ./single.sh
-dnn=lstman4 bs=32  epoch=1 ./single.sh
+dnn=resnet50 bs=256  epoch=1 ./single.sh
+dnn=lstm     bs=256  epoch=1 ./single.sh
+dnn=lstman4  bs=32   epoch=1 ./single.sh
 
